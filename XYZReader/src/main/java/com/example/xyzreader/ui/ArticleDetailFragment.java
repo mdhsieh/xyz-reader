400c563133b865469c42574d863c35398db7d7fb
Michael Hsieh <mdhsieh8@gmail.com>
2020-06-14 12:06:14 -0700

    style: Remove comments and unecessary files.

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -245,7 +245,6 @@ public class ArticleDetailFragment extends Fragment implements
                             Bitmap bitmap = imageContainer.getBitmap();
                             if (bitmap != null) {
                                 Palette p = Palette.generate(bitmap, 12);
-                                // mMutedColor = p.getDarkMutedColor(0xFF333333);
                                 mMutedColor = p.getLightMutedColor(0xFF333333);
                                 mPhotoView.setImageBitmap(imageContainer.getBitmap());
                                 mRootView.findViewById(R.id.meta_bar)

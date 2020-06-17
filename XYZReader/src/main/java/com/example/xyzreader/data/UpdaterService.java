f534a956ce269acffb0ff612f0af485ec811c011
Michael Hsieh <mdhsieh8@gmail.com>
2020-06-16 22:51:26 -0700

    style: Fix typo in comment.

--- XYZReader/src/main/java/com/example/xyzreader/data/UpdaterService.java
+++ XYZReader/src/main/java/com/example/xyzreader/data/UpdaterService.java
@@ -46,7 +46,7 @@ public class UpdaterService extends IntentService {
         sendStickyBroadcast(
                 new Intent(BROADCAST_ACTION_STATE_CHANGE).putExtra(EXTRA_REFRESHING, true));
 
-        // Don't even inspect the intent, we only do onee thing, and that's fetch content.
+        // Don't even inspect the intent, we only do one thing, and that's fetch content.
         ArrayList<ContentProviderOperation> cpo = new ArrayList<ContentProviderOperation>();
 
         Uri dirUri = ItemsContract.Items.buildDirUri();
